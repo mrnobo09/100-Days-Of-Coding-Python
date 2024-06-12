1e6b55b2f60d703aac481f2b43272c262797b616
mrnobo09 <shahabkhanzada999@gmail.com>
2024-06-12 19:32:32 +0500

    First Project of Coding With Python

---
+++ BandNameGen/BandNameGen.py
@@ -0,0 +1,7 @@
+print("Welcome To Band Name Generator!")
+
+city = input("First Input The City You Belong To : ") + "i"
+
+pet = input("Your Favourite Pet Name? : ")
+
+print("\n You Band Name Would Be : "+ city + " " + pet)
