b3654df392e3a6cbddcb8ae658507d2be51a42b1
mrnobo09 <shahabkhanzada999@gmail.com>
2024-06-15 19:56:12 +0500

    Lil Modifications

--- TipCalculator/TipCalculator.py
+++ TipCalculator/TipCalculator.py
@@ -1,8 +1,8 @@
-bill = input("Enter Your Bill: ")
-Persons = input("Number Of Persons: ")
+bill = float(input("Enter Your Bill: "))
+Persons = float(input("Number Of Persons: "))
 
 
-totalBill = float(bill) * 1.12
+totalBill = bill * 1.12
 
 print(f"Total Bill is: {round(totalBill,2)} and each person has to pay {round(totalBill/int(Persons),2)}")
 
